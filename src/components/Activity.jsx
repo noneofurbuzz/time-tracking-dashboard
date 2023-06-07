dd71190a1dd0311bae3fac3c558612380076e4dd
noneofurbuzz <ugochiike26@gmail.com>
2023-06-07 18:06:37 +0100

    Test

--- src/components/Activity.jsx
+++ src/components/Activity.jsx
@@ -2,7 +2,7 @@ export default function Activity(prop){
     return(
         <div className="mx-6 mt-4 ">
             <div className="overflow-hidden relative top-4 py-8 pr-3 rounded-t-2xl" style={{backgroundColor : prop.color}}>
-            <img src={`/src/assets/images/${prop.image}`} className="absolute right-8 -mt-8"></img>
+            <img src={`src/assets/images/${prop.image}`} className="absolute right-8 -mt-8"></img>
             </div>
             <div className={`${prop.darkMode ? "bg-dark-blue" : "bg-random"} flex flex-col justify-between rounded-2xl p-6 relative hover:bg-hover-blue cursor-pointer active:bg-hover-blue`}>
             <div className="flex justify-between items-center">
